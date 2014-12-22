01051ee8a6e4e34c16465303df5eeeede1ff2a65
jin <jin@crypt.sg>
2014-12-22 19:29:17 +0800

    Named loops. Interesting.

---
+++ 29_nested_named_loops.rs
@@ -0,0 +1,22 @@
+fn main() {
+    println!("Start");
+    'level1: loop {
+        println!("Enter level 1");
+        'level2a: loop {
+            println!("Enter level 2a");
+            'level3: loop {
+                println!("Enter level 3");
+                break 'level2a;
+                println!("Exit level 3");
+            }
+            println!("Exit level 2a");
+        }
+        'level2b: loop {
+            println!("Enter level 2b");
+            break 'level1;
+            println!("Exit level 2b");
+        }
+        println!("Exit level 1");
+    }
+    println!("End");
+}
